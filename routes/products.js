d23f5b248b8397248ad47305f54634a09763daba
Henry Lai <e0032134@u.nus.edu>
2022-04-25 08:44:09 +0000

    add try catch block for the products

--- routes/products.js
+++ routes/products.js
@@ -2,14 +2,23 @@ const express = require("express");
 const router = express.Router();
 
 //#1 import in the Product model
-const {Product} = require('../models')
+const { Product } = require('../models')
 
 router.get('/', async (req, res) => {
     // #2 - fetch all the products (ie, SELECT * from products)
-    let products = await Product.collection().fetch();
-    res.render('products/index', {
-        'products': products.toJSON() //#3 convert collection to JSON
-    })
+    try {
+        let products = await Product.collection().fetch();
+        res.render('products/index', {
+            'products': products.toJSON() //#3 convert collection to JSON
+        })
+    } catch (e) {
+        res.status(500);
+        res.json({
+            'message': "Internal server error. Please contact administrator"
+        })
+        console.log(e);
+    }
+
 })
 
 module.exports = router;

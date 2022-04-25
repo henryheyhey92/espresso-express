1bb3b19320ab50f14b127e53dfc15e8262aed065
Henry Lai <e0032134@u.nus.edu>
2022-04-25 07:23:58 +0000

    create products table

---
+++ migrations/20220425071401-products.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db) {
+  return db.createTable('products', {
+    product_id: { type: 'int', primaryKey: true, autoIncrement: true, unsigned: true },
+    product_name: { type: 'string', length: 100, notNull: true },
+    price: {type: 'int', notNull: true},
+    qty: {type: 'int', notNull : true},
+    description: { type: 'string', length: 2000, notNull: false }
+  })
+
+};
+
+exports.down = function (db) {
+  return db.dropTable('products');
+};
+
+exports._meta = {
+  "version": 1
+};

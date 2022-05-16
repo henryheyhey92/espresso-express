b3fed924e39aa001aa503e8c7475ad6cd26ebd8b
Henry Lai <e0032134@u.nus.edu>
2022-05-16 08:25:33 +0000

    prep for deployment

--- index.js
+++ index.js
@@ -130,6 +130,10 @@ async function main() {
 
 main();
 
-app.listen(3000, () => {
+// app.listen(3000, () => {
+//     console.log("Server has started");
+// })
+
+app.listen(process.env.PORT, () => {
     console.log("Server has started");
 })
